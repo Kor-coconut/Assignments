73f086a200ea35d023799ae2287c79c01504d7fd
권정후 <56989261+Kor-coconut@users.noreply.github.com>
2020-10-12 19:54:06 +0900

    Add files via upload

---
+++ 실감프로그램응용및실습/과제3/Assignment_2-2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assingment2_2
+{
+    /*
+    <!-- Virtual로 Tex Overriding 할 것 -->
+    [완] 1. Vehicle 클래스 설계
+    [완] 2. Vehicle 클래스의 name, price property 생성
+    [완] 3. Vehicle 클래스의 Tex Method Virtual 쓸 것
+    [완] 4. Car 클래스 설계
+    [완] 5. Car 클래스의 displacement property 생성
+    [완] 6. Vehicle 클래스의 Tex Method Override 후 구현 (부모의 price 사용)
+    [완] 7. Main 인 Driver 클래스 구현
+    [완] 8. List <Car> 쓸 것
+     */
+    class Vehicle // Vehicle 클래스
+    {
+        private string model_Name;
+        private int model_Price;
+
+        public Vehicle()
+        {
+            model_Name = null;
+            model_Price = 0;
+        }
+
+        public string Name // Name property
+        {
+            get { return model_Name; }
+            set { model_Name = value; }
+        }
+
+        public int Price // Price property
+        {
+            get { return model_Price; }
+            set { model_Price = value; }
+        }
+
+        public virtual double Vehicle_Tex() // For Override
+        {
+            double tex = 0;
+            return tex;
+        }
+    }
+    class Car : Vehicle // Car 클래스
+    {
+        private int displacement;
+        public Car()
+        {
+            displacement = 0;
+        }
+
+        public int Displacement // Displacement
+        {
+            get { return displacement; }
+            set { displacement = value; }
+        }
+
+        public override double Vehicle_Tex() // Tex Override
+        {
+            if(displacement >= 3000) // 배기량 3,000 이상
+            {
+                return base.Price * 0.05;
+            }
+            else if (1500 <= displacement && displacement < 3000) // 배기량 3000 미만 1500 이상
+            {
+                return base.Price * 0.13;
+            }
+            else // 그 외
+            {
+                return base.Price * 0.01;
+            }
+        }
+    }
+
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Anser();
+        }
+
+        static void Anser() // 프로그램 시작
+        {
+            List<Car> myCars = Init();
+            
+            foreach (Car car in myCars) // foreach 문을 통한 List의 Car 클래스 출력
+            {
+                Console.WriteLine("모델명 : {0}, 가격 : {1:#,##0} \\, 배기량 : {2:#,##0} CC, 세금 : {3:#,##0.0###} \\", string.Format(car.Name), car.Price, car.Displacement, car.Vehicle_Tex());
+             }
+        }
+        static List<Car> Init() // 예시로 List 초기화하는 함수
+        {
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car() { Displacement = 3500, Price = 40000000, Name = "제네시스" });
+            cars.Add(new Car() { Displacement = 2000, Price = 30000000, Name = "오필러스" });
+            cars.Add(new Car() { Displacement = 1000, Price = 15000000, Name = "리오"});
+
+            return cars;
+        }
+    }
+}
---
+++ 실감프로그램응용및실습/과제3/Assignment_2-3.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_2_3
+{
+    /*
+    <!-- Books 클래스 멤버로 List<T> 사용 && Property 반드시 사용 -->
+    [완] 1. Books 클래스 생성 --> Book 상속
+    [완] 2. Books 클래스의 AddBook Method(Book) (Book)
+    [완] 3. Books 클래스 멤버의 list 이름은 al
+    [완] 4. Book 클래스 생성 (parameter 2개 생성자 필요)
+    [완] 5. BookCount --> Books의 Property
+    [완] 6. getID, getName Property --> Book의 Property 생성
+    [완] 7. Books 클래스의 ReMoveBook(int) Method (void type)
+    [완] 8. Books 클래스의 BookIsPresent(Book) Method (bool type)
+    [완] 9. Books 클래스의 ClearAllBooks(void) Method (void type);
+
+    4 6 1 3 5 2 7 8 9
+     */
+    class Book
+    {
+        private string Name;
+        private int ID;
+        public Book() // 기본 생성자
+        {
+            Name = null;
+            ID = 0;
+        }
+        public Book(string Name, int ID) // parameter 2개 생성자
+        {
+            this.Name = Name;
+            this.ID = ID;
+        }
+        public string getName // Name의 property
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public int getID // ID의 property
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+    }
+    class Books<T> : Book // Books
+    {
+        private List<T> array_list;
+
+        public Books()
+        {
+            al = new List<T>();
+        }
+
+        public List<T> al
+        {
+            get { return array_list; }
+            set { array_list = value; }
+        }
+
+        public void AddBook(T book)
+        {
+            al.Add(book);
+        }
+
+        public int BookCount
+        {
+            get { return array_list.Count; }
+        }
+
+        public void RemoveBook(int pos)
+        {
+            //al.RemoveAt(array_list.Count - pos); // 마지막으로 들어온 책 중 pos 번째
+            al.RemoveAt(pos); // 0 번째에서 시작해서 pos 번째
+        }
+
+        public bool BookIsPresent(T book)
+        {
+            for (int i = 0; i < al.Count; i++)
+            {
+                if (al[i].Equals(book))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ClearAllBooks()
+        {
+            al.Clear();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books<Book> bookStore = new Books<Book>(); // 문제에는 <Book> 빠져있음. Books의 클래스 멤버로 List<T>를 사용하려면 필요.
+            bookStore.AddBook(new Book("냉정과 열정사이", 10001));
+            bookStore.AddBook(new Book("우리들의 행복한 시간", 10002));
+            bookStore.AddBook(new Book("블루오션 전략", 10003));
+            bookStore.AddBook(new Book("나를 사랑하는 법", 10004));
+
+            Console.WriteLine("You have {0} in the store:\n", bookStore.BookCount);
+
+            foreach (Book b in bookStore.al)
+            {
+                Console.WriteLine("PBSN : {0}", b.getID);
+                Console.WriteLine("Book Name : {0}", b.getName);
+            }
+
+            Console.WriteLine("You remove second book in the store\n");
+
+            bookStore.RemoveBook(2); // 0 번째에서 시작해서 2 번째 책? || 마지막으로 들어온 책 중 두 번째?
+            Console.WriteLine("\nYou have {0} in the store:\n", bookStore.BookCount);
+
+            foreach (Book b in bookStore.al)
+            {
+                Console.WriteLine("PBSN : {0}", b.getID);
+                Console.WriteLine("Book Name : {0}", b.getName);
+            }
+
+            Book temp = new Book("토정비결", 10010);
+            bookStore.AddBook(temp);
+            if (bookStore.BookIsPresent(temp))
+            {
+                Console.WriteLine("{0} is already in the store.", temp.getName);
+            }
+            Console.WriteLine("\nRemove all books in the store.\n");
+
+            bookStore.ClearAllBooks();
+            Console.WriteLine("You have {0} in the store.\n", bookStore.BookCount);
+        }
+    }
+}
